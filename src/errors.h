96f4652d4618b6a61c4b5ec979e7a9925319a4c0
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2015-11-18 22:02:01 +0100

    Create errors.h

---
+++ src/errors.h
@@ -0,0 +1,17 @@
+#ifndef __ERRORS_H__
+#define __ERRORS_H__
+
+#include <exception> // std::exception
+#include <string>    // std::string
+
+class OutOfRangeException : public std::exception {
+public:
+	explicit OutOfRangeException(std::string const& what) : what_(what) {}
+	const char* what() const override {
+		return what_.c_str();
+	}
+private:
+	std::string what_;
+};
+
+#endif // __ERRORS_H__
