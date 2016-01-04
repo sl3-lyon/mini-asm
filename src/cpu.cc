9d3b9a1232d29716d8373882fa8b253816238524
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2016-01-04 15:57:43 +0100

    Update cpu.cc

--- src/cpu.cc
+++ src/cpu.cc
@@ -1,7 +1,5 @@
 #include "cpu.h"
 
-
-
 Stack<0xff> stack{}; //!< @brief Stack
 
 /**
@@ -9,12 +7,12 @@ Stack<0xff> stack{}; //!< @brief Stack
  * @brief Here are the registers used in MiniASM
  */
 namespace registers {
-	u8 A   = 0x00; //!< @brief Accumulator
-	u8 X   = 0x00; //!< @brief X index
-	u8 Y   = 0x00; //!< @brief Y index
-	u8 P   = 0x00; //!< @brief Program status
-	u16 PC = 0x00; //!< @brief Program counter
-	u8 S   = 0x00; //!< @brief Stack pointer
+  u8 A   = 0x00; //!< @brief Accumulator
+  u8 X   = 0x00; //!< @brief X index
+  u8 Y   = 0x00; //!< @brief Y index
+  u8 P   = 0x00; //!< @brief Program status
+  u16 PC = 0x00; //!< @brief Program counter
+  u8 S   = 0x00; //!< @brief Stack pointer
 } // namespace registers
 
 std::vector<u8> ROM(1000);   //!< @brief Buffer in which will be loader the ROM
