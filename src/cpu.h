839ec6488e831d4184c3bae295a6560802fa74e0
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2016-01-05 09:53:13 +0100

    Update cpu.h

--- src/cpu.h
+++ src/cpu.h
@@ -23,37 +23,37 @@ namespace registers {
 } // namespace registers
 
 enum Flags {
-	negative = 0x01,
-	equal = 0x02,
-	lower = 0x03,
-	greater = 0x04
+  negative = 0x01,
+  equal = 0x02,
+  lower = 0x03,
+  greater = 0x04
 }; // namespace flags
 
 template <unsigned size>
 class Stack {
 public:
-	Stack() {
-		static_assert(size > 0, "Stack size must be superior than 0");
-	}
-	void push(u8 value) {
-		assert(registers::S < size && "Stack overflow");
-		if (registers::S >= size) {
-			throw Asm::Errors::OutOfRangeException{"Stack overflow"};
-		}
-		buffer_[registers::S] = value;
-		registers::S++;
-	}
-	u8 pop() {
-		assert(registers::S != 0 && "Stack is empty");
-		if (registers::S == 0) {
-			throw Asm::Errors::OutOfRangeException{"Stack is empty"};
-		}
-		registers::S--;
-		return buffer_[registers::S];
-	}
+  Stack() {
+    static_assert(size > 0, "Stack size must be superior than 0");
+  }
+  void push(u8 value) {
+    assert(registers::S < size && "Stack overflow");
+    if (registers::S >= size) {
+      throw Asm::Errors::OutOfRangeException{"Stack overflow"};
+    }
+    buffer_[registers::S] = value;
+    registers::S++;
+  }
+  u8 pop() {
+    assert(registers::S != 0 && "Stack is empty");
+    if (registers::S == 0) {
+      throw Asm::Errors::OutOfRangeException{"Stack is empty"};
+    }
+    registers::S--;
+    return buffer_[registers::S];
+  }
 
 private:
-	std::array<u8, size> buffer_;
+  std::array<u8, size> buffer_;
 };
 
 extern Stack<0xff> stack;
