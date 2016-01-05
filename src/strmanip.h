ee10c08ebd5113a975f385a900191b1d3e1b5988
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2016-01-05 15:00:24 +0100

    Create strmanip.h

---
+++ src/strmanip.h
@@ -0,0 +1,35 @@
+#ifndef __STRMANIP_H__
+#define __STRMANIP_H__
+
+#include <cctype>  // std::tolower, std::toupper
+#include <string>  // std::string
+
+/**
+  * @brief Turns a string into a lower string
+  * @param str The string we work with
+  * @returns str as lower string
+  * @throw std::bad_alloc if std::string::op+= fails
+  */
+inline std::string to_lower(std::string const& str) {
+  std::string cpy;
+  for (auto const c : str) {
+    cpy += std::tolower(c);
+  }
+  return cpy;
+}
+
+/**
+  * @brief Turns a string into a upper string
+  * @param str The string we work with
+  * @returns str as upper string
+  * @throw std::bad_alloc if std::string::op+= fails
+  */
+inline std::string to_upper(std::string const& str) {
+  std::string cpy;
+  for (auto const c : str) {
+    cpy += std::toupper(c);
+  }
+  return cpy;
+}
+
+#endif // __STRMANIP_H__
